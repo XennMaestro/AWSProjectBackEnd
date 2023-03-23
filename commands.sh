eaee924fb2886c98a12230a93e3a22481dc1d59a
XennMaestro1 <shehryartariq82@hotmail.com>
2023-03-23 15:12:15 -0400

    Added template command and generation files

---
+++ commands.sh
@@ -0,0 +1,8 @@
+# create an s3 bucket
+aws s3 mb s3://awan-code-sam
+
+# package cloudformaiton
+aws cloudformation package --s3-bucket awan-code-sam --template-file template.yaml --output-template-file gen/template-generated.yaml
+
+# deploy 
+aws cloudformation deploy --template-file gen\template-generated.yaml --stack-name back-end-sam --capabilities CAPABILITY_IAM
